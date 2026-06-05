4716b20cdd82dc5245fc240df0c188ae297829ff
bondaruk808-del <bondaruk808@gmail.com>
2026-06-05 18:08:01 +0300

    Теми інтерфейсу

--- TextEditorApp/TextEditorApp/Form1.Designer.cs
+++ TextEditorApp/TextEditorApp/Form1.Designer.cs
@@ -33,25 +33,34 @@ namespace TextEditorApp
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.boldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.italicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.themeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lightThemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkThemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.brotatoThemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.panelLeftWrapper = new System.Windows.Forms.Panel();
+            this.panelLeftBorder = new System.Windows.Forms.Panel();
+            this.panelRightBorder = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.menuStrip1.SuspendLayout();
+            this.panelLeftWrapper.SuspendLayout();
             this.SuspendLayout();
 
-            // MenuStrip 
-            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(50, 50, 50);
-            this.menuStrip1.ForeColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            // menuStrip1
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))));
+            this.menuStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))));
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.formatToolStripMenuItem});
+            this.formatToolStripMenuItem,
+            this.themeToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 3, 0, 3);
             this.menuStrip1.Size = new System.Drawing.Size(900, 30);
             this.menuStrip1.TabIndex = 0;
 
-            // Меню Файл
+            // fileToolStripMenuItem
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.createToolStripMenuItem,
             this.openToolStripMenuItem,
@@ -62,34 +71,39 @@ namespace TextEditorApp
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
             this.fileToolStripMenuItem.Text = "Файл";
 
+            // createToolStripMenuItem
             this.createToolStripMenuItem.Name = "createToolStripMenuItem";
             this.createToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
             this.createToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.createToolStripMenuItem.Text = "Створити";
             this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
 
+            // openToolStripMenuItem
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
             this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.openToolStripMenuItem.Text = "Відкрити...";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
 
+            // saveToolStripMenuItem
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.saveToolStripMenuItem.Text = "Зберегти";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
 
+            // toolStripSeparator2
             this.toolStripSeparator2.Name = "toolStripSeparator2";
             this.toolStripSeparator2.Size = new System.Drawing.Size(221, 6);
 
+            // exitToolStripMenuItem
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.exitToolStripMenuItem.Text = "Вихід";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
 
-            // Меню Формат
+            // formatToolStripMenuItem
             this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fontToolStripMenuItem,
             this.textColorToolStripMenuItem,
@@ -101,73 +115,127 @@ namespace TextEditorApp
             this.formatToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
             this.formatToolStripMenuItem.Text = "Формат";
 
+            // fontToolStripMenuItem
             this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
             this.fontToolStripMenuItem.Size = new System.Drawing.Size(257, 26);
             this.fontToolStripMenuItem.Text = "Шрифт...";
             this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
 
+            // textColorToolStripMenuItem
             this.textColorToolStripMenuItem.Name = "textColorToolStripMenuItem";
             this.textColorToolStripMenuItem.Size = new System.Drawing.Size(257, 26);
             this.textColorToolStripMenuItem.Text = "Колір тексту...";
             this.textColorToolStripMenuItem.Click += new System.EventHandler(this.textColorToolStripMenuItem_Click);
 
+            // backgroundColorToolStripMenuItem
             this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
             this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(257, 26);
             this.backgroundColorToolStripMenuItem.Text = "Колір виділення (фон)...";
             this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click);
 
+            // toolStripSeparator1
             this.toolStripSeparator1.Name = "toolStripSeparator1";
             this.toolStripSeparator1.Size = new System.Drawing.Size(254, 6);
 
+            // boldToolStripMenuItem
             this.boldToolStripMenuItem.Name = "boldToolStripMenuItem";
             this.boldToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
             this.boldToolStripMenuItem.Size = new System.Drawing.Size(257, 26);
             this.boldToolStripMenuItem.Text = "Жирний";
             this.boldToolStripMenuItem.Click += new System.EventHandler(this.boldToolStripMenuItem_Click);
 
+            // italicToolStripMenuItem
             this.italicToolStripMenuItem.Name = "italicToolStripMenuItem";
             this.italicToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.I)));
             this.italicToolStripMenuItem.Size = new System.Drawing.Size(257, 26);
             this.italicToolStripMenuItem.Text = "Курсив";
             this.italicToolStripMenuItem.Click += new System.EventHandler(this.italicToolStripMenuItem_Click);
 
-            // RichTextBox 
-            this.richTextBox1.BackColor = System.Drawing.Color.White;
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11.5F, System.Drawing.FontStyle.Regular);
-            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(30, 30, 30); 
-            this.richTextBox1.Location = new System.Drawing.Point(0, 30);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(0);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Padding = new System.Windows.Forms.Padding(12, 10, 12, 10);
-            this.richTextBox1.Size = new System.Drawing.Size(900, 498);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // themeToolStripMenuItem
+            this.themeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lightThemeToolStripMenuItem,
+            this.darkThemeToolStripMenuItem,
+            this.brotatoThemeToolStripMenuItem});
+            this.themeToolStripMenuItem.Name = "themeToolStripMenuItem";
+            this.themeToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.themeToolStripMenuItem.Text = "Тема";
 
-            // StatusStrip
-            this.statusStrip1.BackColor = System.Drawing.Color.FromArgb(240, 240, 240);
+            // lightThemeToolStripMenuItem
+            this.lightThemeToolStripMenuItem.Name = "lightThemeToolStripMenuItem";
+            this.lightThemeToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
+            this.lightThemeToolStripMenuItem.Text = "Світла";
+            this.lightThemeToolStripMenuItem.Click += new System.EventHandler(this.lightThemeToolStripMenuItem_Click);
+
+            // darkThemeToolStripMenuItem
+            this.darkThemeToolStripMenuItem.Name = "darkThemeToolStripMenuItem";
+            this.darkThemeToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
+            this.darkThemeToolStripMenuItem.Text = "Темна";
+            this.darkThemeToolStripMenuItem.Click += new System.EventHandler(this.darkThemeToolStripMenuItem_Click);
+
+            // brotatoThemeToolStripMenuItem
+            this.brotatoThemeToolStripMenuItem.Name = "brotatoThemeToolStripMenuItem";
+            this.brotatoThemeToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
+            this.brotatoThemeToolStripMenuItem.Text = "Legacy";
+            this.brotatoThemeToolStripMenuItem.Click += new System.EventHandler(this.brotatoThemeToolStripMenuItem_Click);
+
+            // statusStrip1
+            this.statusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))));
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Location = new System.Drawing.Point(0, 528);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(900, 22);
             this.statusStrip1.TabIndex = 2;
 
+            // panelLeftWrapper
+            this.panelLeftWrapper.Controls.Add(this.richTextBox1);
+            this.panelLeftWrapper.Controls.Add(this.panelLeftBorder);
+            this.panelLeftWrapper.Controls.Add(this.panelRightBorder);
+            this.panelLeftWrapper.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLeftWrapper.Location = new System.Drawing.Point(0, 30);
+            this.panelLeftWrapper.Name = "panelLeftWrapper";
+            this.panelLeftWrapper.Size = new System.Drawing.Size(900, 498);
+            this.panelLeftWrapper.TabIndex = 3;
+
+            // panelLeftBorder
+            this.panelLeftBorder.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeftBorder.Location = new System.Drawing.Point(0, 0);
+            this.panelLeftBorder.Name = "panelLeftBorder";
+            this.panelLeftBorder.Size = new System.Drawing.Size(16, 498);
+            this.panelLeftBorder.TabIndex = 1;
+
+            // panelRightBorder
+            this.panelRightBorder.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRightBorder.Location = new System.Drawing.Point(884, 0);
+            this.panelRightBorder.Name = "panelRightBorder";
+            this.panelRightBorder.Size = new System.Drawing.Size(16, 498);
+            this.panelRightBorder.TabIndex = 2;
+
+            // richTextBox1
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11.5F, System.Drawing.FontStyle.Regular);
+            this.richTextBox1.Location = new System.Drawing.Point(16, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(868, 498);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+
             // Form1
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(900, 550);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panelLeftWrapper);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Без імені — Brotato Pad";
+            this.Text = "Без імені — Bro pad";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panelLeftWrapper.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
@@ -186,7 +254,14 @@ namespace TextEditorApp
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem boldToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem italicToolStripMenuItem;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem themeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lightThemeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkThemeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem brotatoThemeToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panelLeftWrapper;
+        private System.Windows.Forms.Panel panelLeftBorder;
+        private System.Windows.Forms.Panel panelRightBorder;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- TextEditorApp/TextEditorApp/Form1.cs
+++ TextEditorApp/TextEditorApp/Form1.cs
@@ -15,12 +15,13 @@ namespace TextEditorApp
         {
             InitializeComponent();
             UpdateFormTitle();
+            ApplyDarkTheme();
         }
 
         private void UpdateFormTitle()
         {
             string asterisk = isFileChanged ? "*" : "";
-            this.Text = $"{asterisk}{currentFileName} — Brotato Pad";
+            this.Text = $"{asterisk}{currentFileName} — Bro pad";
         }
 
         private void createToolStripMenuItem_Click(object sender, EventArgs e)
@@ -60,7 +61,7 @@ namespace TextEditorApp
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.Filter = "Текстові файли (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf|Портативний документ (*.pdf)|*.pdf|Усі файли (*.*)|*.*";
-                openFileDialog.Title = "🥔 Відкрити файл у Brotato Pad";
+                openFileDialog.Title = "🥔 Відкрити файл у Bro pad";
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
@@ -100,7 +101,7 @@ namespace TextEditorApp
             using (SaveFileDialog saveFileDialog = new SaveFileDialog())
             {
                 saveFileDialog.Filter = "Текстові файли (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf|Портативний документ (*.pdf)|*.pdf";
-                saveFileDialog.Title = "🥔 Зберегти файл у Brotato Pad";
+                saveFileDialog.Title = "🥔 Зберегти файл у Bro pad";
                 saveFileDialog.DefaultExt = "txt";
 
                 if (saveFileDialog.ShowDialog() == DialogResult.OK)
@@ -172,7 +173,7 @@ namespace TextEditorApp
             if (isFileChanged)
             {
                 DialogResult result = MessageBox.Show(
-                    "Ви впевнені, що хочете закрити Brotato Pad? Всі незбережені дані будуть втрачені.",
+                    "Ви впевнені, що хочете закрити Bro pad? Всі незбережені дані будуть втрачені.",
                     "Увага!",
                     MessageBoxButtons.YesNo,
                     MessageBoxIcon.Warning
@@ -244,5 +245,119 @@ namespace TextEditorApp
                 richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
             }
         }
+
+        private void lightThemeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ApplyLightTheme();
+        }
+
+        private void darkThemeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ApplyDarkTheme();
+        }
+
+        private void brotatoThemeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ApplyBrotatoTheme();
+        }
+
+        private void ApplyLightTheme()
+        {
+            Color menuBg = Color.FromArgb(240, 240, 240);
+            Color textCol = Color.Black;
+
+            panelLeftWrapper.BackColor = menuBg;
+            panelLeftBorder.BackColor = Color.FromArgb(210, 210, 210);
+            panelRightBorder.BackColor = Color.FromArgb(210, 210, 210);
+
+            richTextBox1.BackColor = Color.White;
+            richTextBox1.ForeColor = Color.FromArgb(30, 30, 30);
+
+            menuStrip1.BackColor = menuBg;
+            menuStrip1.ForeColor = textCol;
+            statusStrip1.BackColor = menuBg;
+            statusStrip1.ForeColor = textCol;
+
+            menuStrip1.Renderer = new ToolStripProfessionalRenderer(new CustomMenuColorTable(menuBg, Color.FromArgb(220, 220, 220), textCol));
+            UpdateMenuTextColors(menuStrip1.Items, textCol);
+        }
+
+        private void ApplyDarkTheme()
+        {
+            Color menuBg = Color.FromArgb(50, 50, 50);
+            Color textCol = Color.White;
+
+            panelLeftWrapper.BackColor = Color.FromArgb(40, 40, 40);
+            panelLeftBorder.BackColor = Color.FromArgb(60, 60, 60);
+            panelRightBorder.BackColor = Color.FromArgb(60, 60, 60);
+
+            richTextBox1.BackColor = Color.FromArgb(25, 25, 25);
+            richTextBox1.ForeColor = Color.FromArgb(245, 245, 245);
+
+            menuStrip1.BackColor = menuBg;
+            menuStrip1.ForeColor = textCol;
+            statusStrip1.BackColor = Color.FromArgb(45, 45, 45);
+            statusStrip1.ForeColor = textCol;
+
+            menuStrip1.Renderer = new ToolStripProfessionalRenderer(new CustomMenuColorTable(menuBg, Color.FromArgb(70, 70, 70), textCol));
+            UpdateMenuTextColors(menuStrip1.Items, textCol);
+        }
+
+        private void ApplyBrotatoTheme()
+        {
+            Color menuBg = Color.FromArgb(59, 47, 41);
+            Color textCol = Color.FromArgb(242, 203, 87);
+
+            panelLeftWrapper.BackColor = Color.FromArgb(44, 34, 30);
+            panelLeftBorder.BackColor = Color.FromArgb(74, 59, 52);
+            panelRightBorder.BackColor = Color.FromArgb(74, 59, 52);
+
+            richTextBox1.BackColor = Color.FromArgb(28, 22, 19);
+            richTextBox1.ForeColor = textCol;
+
+            menuStrip1.BackColor = menuBg;
+            menuStrip1.ForeColor = Color.FromArgb(245, 245, 245);
+            statusStrip1.BackColor = menuBg;
+            statusStrip1.ForeColor = textCol;
+
+            menuStrip1.Renderer = new ToolStripProfessionalRenderer(new CustomMenuColorTable(menuBg, Color.FromArgb(80, 64, 56), textCol));
+            UpdateMenuTextColors(menuStrip1.Items, Color.FromArgb(245, 245, 245));
+        }
+
+        private void UpdateMenuTextColors(ToolStripItemCollection items, Color color)
+        {
+            foreach (ToolStripItem item in items)
+            {
+                item.ForeColor = color;
+                if (item is ToolStripMenuItem menuItem)
+                {
+                    UpdateMenuTextColors(menuItem.DropDownItems, color);
+                }
+            }
+        }
+    }
+
+    public class CustomMenuColorTable : ProfessionalColorTable
+    {
+        private Color backColor;
+        private Color selectionColor;
+        private Color textColor;
+
+        public CustomMenuColorTable(Color backColor, Color selectionColor, Color textColor)
+        {
+            this.backColor = backColor;
+            this.selectionColor = selectionColor;
+            this.textColor = textColor;
+        }
+
+        public override Color ToolStripDropDownBackground => backColor;
+        public override Color MenuBorder => backColor;
+        public override Color MenuItemBorder => selectionColor;
+        public override Color MenuItemSelected => selectionColor;
+        public override Color MenuItemSelectedGradientBegin => selectionColor;
+        public override Color MenuItemSelectedGradientEnd => selectionColor;
+        public override Color ImageMarginGradientBegin => backColor;
+        public override Color ImageMarginGradientMiddle => backColor;
+        public override Color ImageMarginGradientEnd => backColor;
     }
 }
